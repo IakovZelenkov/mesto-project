c91be2e4dfb0fb9d134986003c1357833e436672
Iakov Zelenkov <iakovzelenkov@gmail.com>
2022-12-18 22:44:14 +0300

    finished form validation

--- src/components/data.js
+++ src/components/data.js
@@ -24,3 +24,12 @@ export const initialCards = [
     link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
   },
 ];
+
+export const selectors = {
+  formSelector: ".form",
+  inputSelector: ".form__input",
+  submitButtonSelector: ".form__save-button",
+  inactiveButtonClass: "form__save-button_disabled",
+  inputErrorClass: "form__input_type_error",
+  errorClass: "form__error_active",
+};
